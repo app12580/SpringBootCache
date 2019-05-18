491051f9d2f48a42bca3fb544fedc2f24396ef8a
app12580 <610938672@qq.com>
2019-05-18 17:30:26 +0800

    MongoDBTemplate 用例

--- src/test/java/com/rwt/cache/test/MongoDBTest.java
+++ src/test/java/com/rwt/cache/test/MongoDBTest.java
@@ -2,6 +2,7 @@ package com.rwt.cache.test;
 
 import com.mongodb.DB;
 import com.mongodb.Mongo;
+import com.mongodb.client.result.UpdateResult;
 import com.rwt.cache.CacheApplication;
 import com.rwt.cache.springdata.entity.User;
 import com.rwt.cache.springdata.mongodb.dao.CustomerRepository;
@@ -10,8 +11,16 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
 import java.util.Set;
 
 @RunWith(SpringRunner.class)
@@ -20,7 +29,12 @@ public class MongoDBTest {
 
     @Autowired
     private CustomerRepository repository;
+    @Autowired
+    private MongoTemplate mongoTemplate;
 
+    /**
+     * 原生客户端连接
+     */
     @Test
     public void testMongodb() {
         try {
@@ -40,6 +54,10 @@ public class MongoDBTest {
         }
     }
 
+
+    /**
+     * springdata 整合
+     */
     @Test
     public void testSave() {
         repository.deleteAll();
@@ -65,6 +83,66 @@ public class MongoDBTest {
         System.out.println("--------------------------------");
         for (Customer customer : repository.findByLastName("Smith")) {
             System.out.println(customer);
+        }
+    }
+
+    /**
+     * 测试MongoTemplate
+     */
+    @Test
+    public void mongoTemplateTest() {
+        //增加
+        Customer customer = new Customer("aa", "bb");
+        mongoTemplate.insert(customer);
+
+        //查找
+        Customer find = mongoTemplate.findById(customer.getId(), Customer.class);
+
+        //更新
+        Query query=new Query(Criteria.where("id").is(find.getId()));
+
+        Update update = new Update().set("lastName", "ccc");
+        UpdateResult updateResult = mongoTemplate.updateFirst(query, update, Customer.class);
+        System.out.println(updateResult);
+
+        find = mongoTemplate.findById(customer.getId(), Customer.class);
+        System.out.println("修改后：" + find);
+
+        List<Customer> all = mongoTemplate.findAll(Customer.class);
+        System.out.println("删除前：" + all.size());
+        for(Customer c: all) {
+            System.out.println(c);
+        }
+
+        //删除
+        mongoTemplate.remove(find);
+        List<Customer> all2 = mongoTemplate.findAll(Customer.class);
+        System.out.println("删除后：" + all2.size());
+        for(Customer c: all2) {
+            System.out.println(c);
+        }
+
+        //删除集合
+//        mongoTemplate.dropCollection(Customer.class);
+    }
+
+    @Test
+    public void pageTest() {
+
+        Customer customer = new Customer("aa", "bb");
+        mongoTemplate.insert(customer);
+
+
+        Query query = new Query();
+//        query.addCriteria(Criteria.where("firstName").is("aa"));
+        //query的排序
+        query.with(new Sort(new Sort.Order(Sort.Direction.DESC,"id")));
+        //query的分页
+        Pageable pageable = new PageRequest(0, 5); // get 5 profiles on a page
+        query.with(pageable);
+        List<Customer> customers = mongoTemplate.find(query, Customer.class);
+        for(Customer c: customers) {
+            System.out.println(c);
         }
     }
 }
